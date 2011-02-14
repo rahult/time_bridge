c0f781f61d7d2797504915ac5b72bfe064cee1a3
Rahul Trikha <rahul.trikha@gmail.com>
2011-02-14 17:26:34 +1030

    Added Production mail host

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -46,4 +46,6 @@ TimeBridge::Application.configure do
 
   # Send deprecation notices to registered listeners
   config.active_support.deprecation = :notify
+
+  config.action_mailer.default_url_options = { :host => 'time-bridge.heroku.com' }
 end
